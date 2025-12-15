f25f15d7679a225a913436a2b142e84ad2c7fc93
Naveen <nkumar0333@gmail.com>
2025-12-15 10:03:34 +0530

    y file

---
+++ LoginPage.java
@@ -0,0 +1,11 @@
+package Amazon.MavenProject9pm;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+	WebDriver driver=new ChromeDriver();
+	driver.
+}
+
+}
